92d0c1924a3783980dbcbc062de08cc1aa52e3b0
18006Shakib <18006@lynfield.ac.nz>
2022-04-12 10:25:59 +1200

    Update GUI Tkinter code.py

--- GUI Tkinter code.py
+++ GUI Tkinter code.py
@@ -1,23 +1,41 @@
+#--------------Import Functions---------------------------
 from tkinter import * 
 from functools import partial #to prevent unwanted windows
 
 import random
-
+#---------------First Class-------------------------------
 class Converter:
         def __init__ (self):
-                background_colour= "light blue" #Formatting Variables
                 
-                #Converter Main Screen GUI...
-                self.converter_frame = Frame(width=300, height=300, bg=background_colour)
+#---------------Formatting Variables-----------------------------------------------------------------
+                background_colour= "light blue" 
+                
+#---------------Converter Main Screen GUI...---------------------------------------------------------
+                self.converter_frame = Frame(width=600, height=600, bg=background_colour,
+                                             pady=10)
                 self.converter_frame.grid()
                 
-                # Temperature Conversion Heading (row 0)
+#---------------Temperature Conversion Heading (row 0)-----------------------------------------------
                 self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
                                                    font=("Arial", "16", "bold" ),
                                                    padx=10, pady=10)
                                                    
-                self.temp_converter_label.grid(row=0)                                            
-#main routine
+                self.temp_converter_label.grid(row=0)
+                
+#---------------Help Button (row 1)------------------------------------------------------------------
+                self.help_button = Button(self.converter_frame, text="Help",
+                                          font=("Arial", "16"),
+                                          padx=10, pady=10, comman=self.help)
+                self.help_button.grid(row=1)
+                
+        def help(self):
+                print("You asked for help")
+                get_help = Help(self)
+                get-help.help_text.configure(test="Help text goes here")
+                
+                
+                
+#---------------Main Routine---------------
 if __name__ == "__main__":
         root = Tk()
         root.title("Temperature Converter")
