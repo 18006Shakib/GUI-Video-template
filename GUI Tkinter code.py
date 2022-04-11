0e11485ef762e2ce240b6f9ecb0b41649ece0888
18006Shakib <18006@lynfield.ac.nz>
2022-04-12 09:51:56 +1200

    Create GUI Tkinter code.py

---
+++ GUI Tkinter code.py
@@ -0,0 +1,16 @@
+from tkinter import * 
+#import random
+
+
+class Converter:
+        def __init__ (self, parent):
+                print("hello world")
+        
+        
+        
+#main routine
+if __name__ == "_main_":
+        root = Tk()
+        root.title('Temperature Converter')
+        something = Converter(root)
+        root.mainloop()
